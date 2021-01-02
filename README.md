6b8cee1d9b3e5bb072e8f5a719a9b7aa7f70d05d
Fezzah <festusabiola39@gmail.com>
2021-01-02 12:06:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5qyrd9-ba7evr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5qyrd9-ba7evr)
---
+++ src/app/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+  {
+    image:'https://copia.co.ke/wp-content/uploads/2019/08/Royco-Mchuzi-Mix-Beef-200g_Seasoning_855_1.jpeg',
+    name: 'Royco Mchuzi Mix Beef 200g',
+    price: 135,
+    description: 'Order online at Abinuzi Super Mart and enjoy free delivery to Mashinani or City Outskirts in two to four days.'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
